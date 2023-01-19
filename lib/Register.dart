3d21e972acf166e8cf8d83d2d9b08c2c76db0d28
Ao5 <632021129@tsu.ac.th>
2023-01-19 16:36:14 +0700

    Hello129

---
+++ lib/Register.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class RegisterPages extends StatefulWidget {
+  const RegisterPages({super.key});
+
+  @override
+  State<RegisterPages> createState() => _RegisterPagesState();
+}
+
+class _RegisterPagesState extends State<RegisterPages> {
+  final _formkey = GlobalKey<FormState>();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      body: SafeArea(
+          child: Form(
+        key: _formkey,
+        child: ListView(
+          children: [
+            TextFormField(
+                controller: _emailController,
+                validator: ((value) {
+                  if (value!.isEmpty) {
+                    return "Enter Email Please!";
+                  }
+                  return null;
+                })),
+            TextFormField(
+              controller: _passwordController,
+              validator: ((value) {
+                if (value!.isEmpty) {
+                  return "Enter Password Please ";
+                }
+                return null;
+              }),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  if (_formkey.currentState!.validate()) {
+                    print("OK");
+                  }
+                },
+                child: const Text("Register")),
+          ],
+        ),
+      )),
+    );
+  }
+}
---
+++ lib/loginpage.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:lab06/Register.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formkey = GlobalKey<FormState>();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      body: SafeArea(
+          child: Form(
+        key: _formkey,
+        child: ListView(
+          children: [
+            TextFormField(
+                controller: _emailController,
+                validator: ((value) {
+                  if (value!.isEmpty) {
+                    return "Enter Email Please!";
+                  }
+                  return null;
+                })),
+            TextFormField(
+              controller: _passwordController,
+              validator: ((value) {
+                if (value!.isEmpty) {
+                  return "Enter Password Please ";
+                }
+                return null;
+              }),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  if (_formkey.currentState!.validate()) {
+                    print("OK");
+                  }
+                },
+                child: const Text("Login")),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => RegisterPages()));
+                },
+                child: const Text("Register")),
+          ],
+        ),
+      )),
+    );
+  }
+}
